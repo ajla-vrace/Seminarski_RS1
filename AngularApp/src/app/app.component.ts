d722ff92297619bc343d3a2e30f1a5833c629b57
Ajla <ajla.vrace@edu.fit.ba>
2022-12-22 23:07:00 +0100

    angular app

--- AngularApp/src/app/app.component.ts
+++ AngularApp/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import{Router} from "@angular/router";
+import {HttpClient} from "@angular/common/http";
+import {MojConfig} from "./moj-config";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements  OnInit{
+  ngOnInit(): void {
+      throw new Error('Method not implemented.');
+  }
   title = 'AngularApp';
+
+
+  pocetna() {
+
+  }
+
+  otvoriFaq() {
+
+  }
+
+  otvoriHelp() {
+   
+  }
 }
--- AngularApp/src/app/app.module.ts
+++ AngularApp/src/app/app.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from "@angular/forms";
-
+import {RouterModule} from "@angular/router";
+import {HttpClient} from "@angular/common/http";
 import { AppComponent } from './app.component';
+import { FaqComponent } from './faq/faq.component';
+import { Help1Component } from './help1/help1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FaqComponent,
+    Help1Component
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot([
+    {path: 'faq', component: FaqComponent},
+    {path: 'help1', component: Help1Component},
+  ]),
     FormsModule
   ],
   providers: [],
