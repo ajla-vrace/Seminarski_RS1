32bb5ab6a0bedd630222777f99b73cd0bb07fd96
Ajla <ajla.vrace@edu.fit.ba>
2022-12-24 13:08:39 +0100

    telefon

--- OnlineShop/OnlineShop/Modul1/Models/Korisnik.cs
+++ OnlineShop/OnlineShop/Modul1/Models/Korisnik.cs
@@ -10,7 +10,7 @@ namespace OnlineShop.Modul1.Models
         public string Username { get; set; }
         public string Lozinka { get; set; }
         public string Email { get; set; }
-        public string BrojTelefona {get; set; }
+        public string? BrojTelefona {get; set; }
         public DateTime DatumRegistracije { get; set; }
         public bool? isAdmin { get; set; }
         public bool? isZaposlenik { get; set; }
