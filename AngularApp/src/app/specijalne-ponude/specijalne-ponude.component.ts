cc9796c2245c1ba86e2c8425d5a26e97a8c0f6bc
Ajla <ajla.vrace@edu.fit.ba>
2023-07-10 00:34:48 +0200

    signal R

--- AngularApp/src/app/specijalne-ponude/specijalne-ponude.component.ts
+++ AngularApp/src/app/specijalne-ponude/specijalne-ponude.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {SignalRProba1Servis} from "../_servisi/SignalRProba1Servis";
-import {SignalRProba2Servis} from "../_servisi/SignalRProba2Servis";
+
 @Component({
   selector: 'app-specijalne-ponude',
   templateUrl: './specijalne-ponude.component.html',
@@ -8,10 +7,7 @@ import {SignalRProba2Servis} from "../_servisi/SignalRProba2Servis";
 })
 export class SpecijalnePonudeComponent implements OnInit {
 
-  constructor(public probaServis1: SignalRProba1Servis, public probaServis2: SignalRProba2Servis) {
-    probaServis1.otvoriKanalWebSocket();
-    probaServis2.otvoriKanalWebSocket();
-  }
+
 
   ngOnInit(): void {
 
